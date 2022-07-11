30c4c44a65e8c33db04acd2c79b605f0de3f09db
Abiyyu Yafi <yafiabiyyu@protonmail.com>
2022-07-11 12:04:30 +0700

    update & remove mainnet-fork config

--- scripts/help_scripts.py
+++ scripts/help_scripts.py
@@ -1,4 +1,4 @@
-from brownie import accounts, config, network
+from brownie import Contract, accounts, config, network, web3
 
 
 def get_account():
@@ -6,6 +6,13 @@ def get_account():
         return accounts
     else:
         account = accounts.from_mnemonic(
-            mnemonic=config['wallets']['from_mnemonic'],count=5
+            mnemonic=config["wallets"]["from_mnemonic"], count=5
         )
         return account
+
+
+def transferEther():
+    transferAmount = web3.toWei("1.2", "ether")
+    main = get_account()[0]
+    second = get_account()[1]
+    main.transfer(second, transferAmount)
